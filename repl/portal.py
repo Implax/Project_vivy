1ee5de086b1cfc135ae95efe9a64c05220a26ac4
Implax <juniourclottey@gmail.com>
2022-04-28 20:14:03 +0000

    Wiki Engine

--- repl/portal.py
+++ repl/portal.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
+import webbrowser as web
+import wikipedia
 
 listener = sr.Recognizer() #Recognizer Class helps to recognize speech
 engine = pyttsx3.init()
@@ -21,8 +23,8 @@ def take_command():
             command = listener.recognize_google(voice)
             command = command.lower()
 
-            if 'vivi' in command:
-                print(command)
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
         
     except:
         pass
@@ -30,8 +32,22 @@ def take_command():
 
 def run_vi():
     command = take_command()
-    if 'hey' in command:
-        talk("Hi I'm Vivi. Welcome to the E-Academic Advisory system.")
+    if 'hi' or 'hey' in command:
+        talk("Hi I'm Alexa. Welcome to the E-Academic Advisory system. If you want to know more about me and how I work with the entire system. Say general overview")
+    elif 'general overview' in command:
+        talk("The E-Academic System is designed to assist Ashesi students with easy navigation of courses pertaining to their majors and current academic standings. It utilizes information from each student's data to tailor usage of the platform to students' best interests.")
+    elif 'describe' in command:
+        thing = command.replace('describe', '')
+        
+        description = wikipedia.summary(thing, 1)
+        print(description)
+        talk(description)
+    elif 'electives' and 'Computer Science' in command:
+        talk('Opening the Ashesi website to view electives')
+        url = 'https://www.ashesi.edu.gh/academics/programmes/arts-and-sciences/liberal-arts-core-courses.html'
+        web.open(url)
+    else:
+        talk('Kindly repeat your command.')
 
 while True:
     run_vi()
